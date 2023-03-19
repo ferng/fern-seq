3d11308ca69ecdb0083cf464942544b003aac264
ferng001 <ferng001@gmail.com>
2023-03-19 13:34:05 +0000

    refactor ambika settings

--- seq4.sh
+++ seq4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-puredata -noaudio -alsamidi -midiindev "1,2,3" -midioutdev "1,2,3" -open Documents/Pd/fern-seq/seq4.pd  &
+puredata -noaudio -alsamidi -midiindev "1,2,3" -midioutdev "1,2,3" -open ~/Documents/Pd/fern-seq/seq4.pd  &
 #puredata -nogui -noaudio -alsamidi -midiindev "1,2" -midioutdev "1,2" -open seq3.pd  &
 
 sleep 3
