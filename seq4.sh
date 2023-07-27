c1fef0daf2462e638e50de7b7e3defa5f5f89e33
ferng001 <ferng001@gmail.com>
2023-07-27 14:30:48 +0000

    move zoom to its own port

--- seq4.sh
+++ seq4.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 pkill puredata
-puredata -noaudio -alsamidi -midiindev "1,2,3" -midioutdev "1,2,3" -open ~/Documents/Pd/fern-seq/seq4.pd  &
-#puredata -nogui -noaudio -alsamidi -midiindev "1,2" -midioutdev "1,2" -open seq3.pd  &
+puredata -noaudio -alsamidi -midiindev "1,2,3,4" -midioutdev "1,2,3,4" -open ~/Documents/Pd/fern-seq/seq4.pd  &
 
 sleep 3
 
