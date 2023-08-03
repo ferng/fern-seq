86b65bb2d4dea88f3b8ee99691b080edf2d01ea6
ferng001 <ferng001@gmail.com>
2023-08-03 16:50:17 +0000

    refactor program change. tune

--- seq-no-gui.sh
+++ seq-no-gui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pkill puredata
-puredata -noaudio -nogui -alsamidi -midiindev "1,2,3" -midioutdev "1,2,3" -open ~/Documents/Pd/fern-seq/seq4.pd  &
+puredata -noaudio -nogui -alsamidi -midiindev "1,2,3,4" -midioutdev "1,2,3,4" -open ~/Documents/Pd/fern-seq/seq4.pd  &
 #puredata -nogui -noaudio -alsamidi -midiindev "1,2" -midioutdev "1,2" -open seq3.pd  &
 
 sleep 3
