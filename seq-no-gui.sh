8ecc81abbea0e4e657a48d53adf1be3fff136403
ferng001 <ferng001@gmail.com>
2024-05-26 17:19:23 +0000

    tune, volca drum routings

--- seq-no-gui.sh
+++ seq-no-gui.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 pkill puredata
-puredata -noaudio -audiobuf 1 -r 192000 -sleepgrain 0.1 -rt -no-gui -alsamidi -midiindev "1,2,3,4" -midioutdev "1,2,3,4" -open ~/Documents/Pd/fern-seq/seq4.pd  &
-#puredata -nogui -noaudio -alsamidi -midiindev "1,2" -midioutdev "1,2" -open seq3.pd  &
+puredata -noaudio -audiobuf 1 -r 192000 -sleepgrain 0.1 -rt -alsamidi -midiindev "1,2,3,4" -midioutdev "1,2,3,4" -nogui -open ~/Documents/Pd/fern-seq/seq4.pd  &
 
 sleep 3
 
