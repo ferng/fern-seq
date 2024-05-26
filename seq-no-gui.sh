54508c8d5cfe18fdcd25e7ba3e80e41616d65c94
ferng001 <ferng001@gmail.com>
2024-05-26 17:14:24 +0000

    reduce latency

--- seq-no-gui.sh
+++ seq-no-gui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pkill puredata
-puredata -noaudio -nogui -alsamidi -midiindev "1,2,3,4" -midioutdev "1,2,3,4" -open ~/Documents/Pd/fern-seq/seq4.pd  &
+puredata -noaudio -audiobuf 1 -r 192000 -sleepgrain 0.1 -rt -no-gui -alsamidi -midiindev "1,2,3,4" -midioutdev "1,2,3,4" -open ~/Documents/Pd/fern-seq/seq4.pd  &
 #puredata -nogui -noaudio -alsamidi -midiindev "1,2" -midioutdev "1,2" -open seq3.pd  &
 
 sleep 3
--- seq4.sh
+++ seq4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pkill puredata
-puredata -noaudio -alsamidi -midiindev "1,2,3,4" -midioutdev "1,2,3,4" -open ~/Documents/Pd/fern-seq/seq4.pd  &
+puredata -noaudio -audiobuf 1 -r 192000 -sleepgrain 0.1 -rt -alsamidi -midiindev "1,2,3,4" -midioutdev "1,2,3,4" -open ~/Documents/Pd/fern-seq/seq4.pd  &
 
 sleep 3
 
