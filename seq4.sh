7bf5fe04fce9e18bf8b9a1dcce92f037ae632c7e
ferng001 <ferng001@gmail.com>
2024-10-27 11:33:03 +0000

    add umidi. remove tr08 and smpltrek

--- seq4.sh
+++ seq4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pkill puredata
-puredata -noaudio -audiobuf 1 -r 192000 -sleepgrain 0.1 -rt -alsamidi -midiindev "1,2,3,4,5" -midioutdev "1,2,3,4,5" -open ~/Documents/Pd/fern-seq/seq4.pd  &
+puredata -noaudio -audiobuf 1 -r 192000 -sleepgrain 0.1 -rt -alsamidi -midiindev "1,2,3,4,5,6" -midioutdev "1,2,3,4,5,6" -open ~/Documents/Pd/fern-seq/seq4.pd  &
 
 sleep 3
 
